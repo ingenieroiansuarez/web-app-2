4cab469fbaae1792820a2031acb3c77e3e781df1
IAN <ingenieroiansuarez@gmail.com>
2025-05-02 23:19:26 -0500

    add upload feature

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 import streamlit as st
 from PIL import Image
 
+st.subheader("Grayscale Image Converter")
+## This is a simple image converter that converts an image to grayscale
+# create a file uploader
+uploaded_file = st.file_uploader("Choose an image file",
+                                 type=['jpg', 'jpeg', 'png'])
+#check if the user has uploaded a file
+if uploaded_file:
+    # open with PIL
+    img = Image.open(uploaded_file)
+    #convert to grayscale
+    gray_img = img.convert('L')
+    #display the image
+    st.image(gray_img)
+
+st.write("---")
 with st.expander("Star camera"):
-#start the camara
+    st.write("Take a photo to convert to grayscale")
     camera_image = st.camera_input('Camera')
 
 if camera_image:
@@ -13,3 +28,5 @@ if camera_image:
     gray_img = img.convert('L')
     #render the image
     st.image(gray_img)
+
+ 
